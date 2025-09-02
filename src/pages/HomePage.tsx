1fe536da07a74f6a89e7789d5742f3bfa9771656
Mark <menachemsalzberg13@gmail.com>
2025-09-02 15:28:11 +0300

    feat: add Header component and integrate it into HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,9 @@
+import Header from "../components/Header";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Home Page!!</h1>
+      <Header headerText="Home Page" btnText="Light/Dark mode" />
     </div>
   );
 }
