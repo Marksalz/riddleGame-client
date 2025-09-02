dad4e35d8a0c605450471e6da00cf1a02a199a0d
Mark <menachemsalzberg13@gmail.com>
2025-09-02 15:16:00 +0300

    feat: add routing with react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import MenuPage from "./pages/MenuPage";
+import GamePage from "./pages/GamePage";
+import GameData from "./pages/GameData";
+import LeaderboardPage from "./pages/LeaderboardPage";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/game" element={<GamePage />} />
+        <Route path="/game_data" element={<GameData />} />
+        <Route path="/leaderboard" element={<LeaderboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
